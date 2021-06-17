9128afad7f3a6b59dc38ad427a1279f081754ab9
Daniel Mattox <dmattox10@Daniels-MacBook-Pro.local>
2021-06-17 13:58:36 -0400

    Added auth to testing db mongo definition

--- env.js
+++ env.js
@@ -12,11 +12,12 @@ const {
 let MONGO_URI = (env = ENVIRONMENT) => {
     console.log(env)
     let URI = ''
+    const { MONGO_HOST, MONGO_PORT, MONGO_USER, MONGO_PASS, DATABASE } = process.env
     if (env === 'testing') {
-        URI = 'mongodb://mongo:27017/auth'
+        URI = `mongodb://${MONGO_USER}:${MONGO_PASS}@mongo:27017/auth?authSource=admin`
     } else if (env === 'prod') {   
-        const { MONGO_HOST, MONGO_PORT, MONGO_USER, MONGO_PASS } = process.env
-        URI = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/auth?authSource=admin`
+        
+        URI = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${DATABASE}?authSource=admin`
     } else {
         URI = 'mongodb://localhost:27017/auth'
     }
